96452dc4c56d0578568fafb5b22683e6f3ac331c
Mia Salaveria <miasalaveria@gmail.com>
2025-05-01 20:26:35 -0400

    fixing mobile and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -20,53 +20,53 @@ export default function Home() {
     <>
       <Head>
         <title>Mia Salaveria Portfolio</title>
-        <meta name="description" content="Welcome to the Next.js starter template" />
+        <meta name="description" content="Front End Developer Portfolio of Mia Salaveria" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="">
         <nav className="row flex justify-center py-[24px] border border-white">
           <div className="container flex justify-between items-center">
-            <h1 className="monument-black text-[24px]">MIA<span className="monument-regular">SALAVERIA</span></h1>
-            <a className="monument-regular text-[18px]" href="/files/MiaSalaveria2025Resume.pdf" download>RESUME</a>
+            <h1 className="monument-black text-[18px] lg:text-[24px]">MIA<span className="monument-regular">SALAVERIA</span></h1>
+            <a className="monument-regular text-[12px] lg:text-[18px]" href="/files/MiaSalaveria2025Resume.pdf" download>RESUME</a>
           </div>
         </nav>
         <main className="relative">
           <section className="w-full border border-white flex justify-center">
             <div className="container flex">
-              <div className="flex justify-start flex-wrap w-[60%] border-r border-white">
-                <div className="text w-[60%] flex justify-center flex-col pr-[32px] border-r border-white">
+              <div className="flex justify-start flex-wrap w-full xl:w-[60%] md:border-r border-white ">
+                <div className="text w-full md:w-[60%] flex justify-center flex-col pr-[32px] py-[24px] md:border-r border-white">
                   <h2 className="monument-black text-[34px] mb-[24px] leading-none"><span className="monument-regular">FRONT-END</span> DEVELOPER</h2>
                   <h3 className="max-w-[700px] mt-[16px] text-[14px]">Hi! I'm Mia and I'm a front-end developer with 7 years of experience building and maintaining web applications helping companies generate leads on marketing websites. A creative at heart, I dabble in different types of interactive media mainly web, design, and music.</h3>
                 </div>
-                <div className="image-wrapper p-[16px] w-[40%]">
+                <div className="image-wrapper px-none py-[16px] md:p-[16px] w-full md:w-[40%]">
                   <div className="h-[300px] relative overflow-hidden">
                     <Image src="/images/mia-salaveria.jpg" alt="Profile" fill style={{ objectFit: 'cover' }} />
                   </div>
                 </div>
               </div>
-              <div className="w-[40%] p-[16px]">
+              <div className="w-[40%] p-[16px] hidden xl:block">
                 <div className="h-full relative overflow-hidden">
                   <Image src="/images/black-texture.jpg" alt="Profile" fill style={{ objectFit: 'cover' }} />
                 </div>
               </div>
             </div>
           </section>
-          <section className="w-full border border-r border-l border-b border-white flex justify-center">
-            <div className="container flex">
-              <div className="flex justify-start flex-wrap w-[55%] border-r border-white relative overflow-hidden">
+          <section className="w-full border-r border-l border-b border-white flex justify-center">
+            <div className="container flex flex-wrap">
+              <div className="flex justify-start flex-wrap  w-full lg:w-[55%] lg:border-r border-b lg:border-b-0 border-white relative overflow-hidden">
                 <Image src="/images/black-texture.jpg" alt="Profile" fill style={{ objectFit: 'cover' }} />
-                <div className="projects flex w-full flex-wrap relative z-2 border-l border-white">
+                <div className="projects flex w-full flex-wrap relative z-2 border-l border-r lg:border-r-0 border-white">
                   <div className="header-bar w-full p-4 bg-black">
                     <h2 className="text-left monument-regular text-[18px] text-white">+ WORK</h2>
                   </div>
-                  <div className="h-[400px] w-[50%] cursor-pointer" onClick={() => openModal(0)}>
+                  <div className="h-[400px] w-full md:w-[50%] cursor-pointer" onClick={() => openModal(0)}>
                     <div className="h-full w-full p-[16px]  border-t border-white">
                       <div className="image-wrapper bg-black h-full w-full flex items-center justify-center hover:bg-white transition duration-400 ease-in-out">
-                        <Image src="/images/tb-logo.png" alt="Profile" width={0} style={{ width: 'auto' }} height={70} sizes="100vw" />
+                        <Image src="/images/tb-logo.png" alt="Profile" width={0} style={{ width: 'auto', height: '40px' }} height={70} sizes="100vw" />
                       </div>
                     </div>
                   </div>
-                  <div className="h-[400px] w-[50%] cursor-pointer" onClick={() => openModal(1)}>
+                  <div className="h-[400px] w-full md:w-[50%] cursor-pointer" onClick={() => openModal(1)}>
                     <div className="h-full w-full p-[16px] border-l border-t border-white">
                       <div className="image-wrapper bg-black h-full w-full flex items-center justify-center hover:bg-white transition duration-400 ease-in-out">
                         <Image src="/images/relay-logo.png" alt="Profile" width={0} style={{ width: 'auto', height: '60px' }} height={80} sizes="100vw" />
@@ -75,7 +75,7 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-              <div className="flex justify-start flex-wrap w-[45%]">
+              <div className="flex justify-start flex-wrap w-full lg:w-[45%] border-r border-l lg:border-r-0 lg:border-l-0 border-white">
                 <div className="tools w-full flex justify-center flex-col py-[20px] pl-[32px]">
                   <h2 className="monument-regular text-[18px] mb-[24px]">+ TOOLS</h2>
                   <ul className="flex flex-wrap pl-0">
@@ -114,10 +114,10 @@ export default function Home() {
                 <div className="modal-content p-6 max-h-[700px] overflow-y-scroll scrollbar-hide">
                   {activeProject === 0 && (
                     <div className="flex flex-wrap">
-                      <div className="w-[50%] h-[200px] bg-[#eee] flex justify-center items-center">
+                      <div className="w-full md:w-[50%] h-[200px] bg-[#eee] flex justify-center items-center">
                         <Image src="/images/tb-logo.png" alt="Profile" width={0} style={{ width: 'auto', height: '40px' }} height={60} sizes="100vw" />
                       </div>
-                      <div className="w-[50%] flex flex-col justify-center px-6">
+                      <div className="w-full md:w-[50%] mt-4 md:mt-0 flex flex-col justify-center px-6">
                         <h2 className="text-xl font-bold mb-4 monument-regular uppercase text-center">TouchBistro</h2>
                         <div className="flex justify-center"><a href="https://touchbistro.com/" target="_blank" className="tool-sticker hover:bg-white transition duration-400 ease-in-out hover:!text-black">Visit</a></div>
                       </div>
@@ -160,10 +160,10 @@ export default function Home() {
                   )}
                   {activeProject === 1 && (
                     <div className="flex flex-wrap">
-                      <div className="w-[50%] h-[200px] bg-[#eee] flex justify-center items-center">
+                      <div className="w-full md:w-[50%] h-[200px] bg-[#eee] flex justify-center items-center">
                         <Image src="/images/relay-logo.png" alt="Profile" width={0} style={{ width: 'auto', height: '50px' }} height={80} sizes="100vw" />
                       </div>
-                      <div className="w-[50%] flex flex-col justify-center px-6">
+                      <div className="w-full md:w-[50%] mt-4 md:mt-0 flex flex-col justify-center px-6">
                         <h2 className="text-xl font-bold mb-4 monument-regular uppercase text-center">Relay Financial</h2>
                         <div className="flex justify-center"><a href="https://relayfi.com/scaling-businesses" target="_blank" className="tool-sticker hover:bg-white transition duration-400 ease-in-out hover:!text-black">Visit</a></div>
                       </div>
@@ -198,7 +198,7 @@ export default function Home() {
           )}
         </main>
         <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center p-6">
-          <p className="text-[12px] text-center font-bold monument-regular ">COPYRIGHT © 2025 MIA SALAVERIA</p>
+          <p className="text-[10px] text-center font-bold monument-regular ">COPYRIGHT © 2025 MIA SALAVERIA</p>
         </footer>
       </div>
     </>
